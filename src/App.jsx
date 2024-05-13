0b2195a451659fc89b10dc156dcb3915b70158b5
diwanumang <diwan.umang@gmail.com>
2024-05-12 21:55:28 -0400

    Continuing to work on the lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,134 @@
 // src/App.jsx
+import { useState } from "react";
+import './App.css'
 
 const App = () => {
+  const [team, setTeam] = useState([]);
+  const [money, setMoney] = useState(100);
+  const [zombieFighter, setZombieFighter] = useState([
+    {
+      name: 'Survivor',
+      price: 12,
+      strength: 6,
+      agility: 4,
+      img: 'https://via.placeholder.com/150/92c952',
+    },
+    {
+      name: 'Scavenger',
+      price: 10,
+      strength: 5,
+      agility: 5,
+      img: 'https://via.placeholder.com/150/771796',
+    },
+    {
+      name: 'Shadow',
+      price: 18,
+      strength: 7,
+      agility: 8,
+      img: 'https://via.placeholder.com/150/24f355',
+    },
+    {
+      name: 'Tracker',
+      price: 14,
+      strength: 7,
+      agility: 6,
+      img: 'https://via.placeholder.com/150/d32776',
+    },
+    {
+      name: 'Sharpshooter',
+      price: 20,
+      strength: 6,
+      agility: 8,
+      img: 'https://via.placeholder.com/150/1ee8a4',
+    },
+    {
+      name: 'Medic',
+      price: 15,
+      strength: 5,
+      agility: 7,
+      img: 'https://via.placeholder.com/150/66b7d2',
+    },
+    {
+      name: 'Engineer',
+      price: 16,
+      strength: 6,
+      agility: 5,
+      img: 'https://via.placeholder.com/150/56acb2',
+    },
+    {
+      name: 'Brawler',
+      price: 11,
+      strength: 8,
+      agility: 3,
+      img: 'https://via.placeholder.com/150/8985dc',
+    },
+    {
+      name: 'Infiltrator',
+      price: 17,
+      strength: 5,
+      agility: 9,
+      img: 'https://via.placeholder.com/150/392537',
+    },
+    {
+      name: 'Leader',
+      price: 22,
+      strength: 7,
+      agility: 6,
+      img: 'https://via.placeholder.com/150/602b9e',
+    },
+  ]
+  );
+
+  const handleAddFighter = (addFighter) => {
+    if (money >= addFighter.price) {
+      setTeam([...team, addFighter])
+      setMoney(money - addFighter.price)
+    } else {
+      console.log('Not enough money');
+    }
+  };
 
   return (
-    <h1>Hello world!</h1>
+    <>
+    <h1>Zombie Fighters!</h1>
+
+    <h2>Money: {money}</h2>
+
+    <div className="playerTeam">
+      <h2>Team</h2>
+        {team.length === 0 ? (
+          <p>Pick some team members!</p>
+        ) : (
+          <ul>
+            {team.map((fighter, index) => (
+              <li key={index}>
+                <img src={fighter.img} alt="#" />
+                <p>{fighter.name}</p>
+                <p>Price: {fighter.price}</p>
+                <p>Strength: {fighter.strength}</p>
+                <p>Agility: {fighter.agility}</p>
+              </li>
+            ))}
+          </ul>
+        )}
+    </div>
+
+    <div className="fighterList">
+      <h2>Fighters</h2>
+        <ul>
+          {zombieFighter.map((fighter, index) => (
+            <li key={index}>
+              <img src={fighter.img} alt="#" />
+              <p>{fighter.name}</p>
+              <p>Price: {fighter.price}</p>
+              <p>Strength: {fighter.strength}</p>
+              <p>Agility: {fighter.agility}</p>
+              <button onClick={() => handleAddFighter(fighter)}>Add</button>
+            </li>
+          ))}
+        </ul>
+    </div>
+    </>
   );
 }
 
